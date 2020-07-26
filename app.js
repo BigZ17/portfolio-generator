2963cebe72e43630d5521e4e95a3a9ea31449753
wacky@Zacs-Desktop <zcraven17@gmail.com>
2020-07-25 22:33:23 -0700

    Stater code

---
+++ app.js
@@ -0,0 +1,27 @@
+// var message = 'Hello World!';
+
+// var sum = 5 + 3;
+
+// console.log(message);
+// console.log(sum);
+
+//var commandLineArgs = process.argv.slice(2, process.argv.length);
+//console.log(commandLineArgs);
+
+
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+// Notice the lack of parentheses around the 'profileDataArr' parameter?
+
+const printProfileData = profileDataArr => {
+    for  (let i=0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+
+        console.log('================');
+        profileDataArr.forEach(profileItem => console.log(profileItem));
+        };
+        
+  };
+  
